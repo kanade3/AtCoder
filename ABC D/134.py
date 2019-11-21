2f3c13b62178cfc22927397ee510451153464bc3
kzmaro <maronkazu94@gmail.com>
2019-11-21 18:42:22 +0900

    134 ボール箱入れのやつ。出力かなりミスった。 144再,水溢れatanのやつ

--- ABC D/134.py
+++ ABC D/134.py
@@ -2,15 +2,19 @@ n = int(input())
 a = list(map(int, input().split()))
 c = [0]
 d = c + a
-v = [0] * (n+1)
-for i in range(n, 1, -1):
-    s = 0
-    for j in range(2 * i, n + 1, i):
-        if i % j == 0:
-            s ^= v[j]
-    d[i] = (s ^ v[i])
+s = [0 for _ in range(n + 1)]
 
-print(d.count(0))
-for i in range(1, len(d)):
-    if d[i] != 0:
+for i in range(n, 0, -1):
+    # 今注目してるiについて、その倍数に当たる部分の球の和を合わせる。そのための変数wを定義する
+    w = 0
+    for j in range(2 * i, n + 1, i):
+        # 2で割ったあまりなのでXORをとる
+        # wは現段階での格納されている個数(あまりで表現される)
+        w ^= s[j]
+    # wと入力(i)を一致させ,出力用のsに格納する
+    s[i] = (w ^ d[i])
+
+print(sum(s))
+for i in range(1, n + 1):
+    if s[i] != 0:
         print(i, end=' ')
